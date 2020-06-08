2bb54795e09a01f6d9dc22137d8d8a15107c7ff3
Satoshi Morita <morita@issp.u-tokyo.ac.jp>
2020-06-08 23:42:39 +0900

    Update a page of the Ising model

--- ising.js
+++ ising.js
@@ -1,376 +1,298 @@
-function Ising() {
-    var l=100;
-    var n = l*l;
-    var tc = 2.0/(Math.log(Math.sqrt(2.0)+1.0)); // T_c(SQ)
-    var temp = tc;
-    var algorithm = 0;
+class Ising {
+  constructor(Lx, Ly, T) {
+    this.T = T;
+    this.resize(Lx, Ly);
+  }
 
-    var running = false;
-    var timerID;
-    var basetime = 0;
-    var interval = 40; // [msec]
+  resize(Lx, Ly) {
+    this.Lx = Lx;
+    this.Ly = Ly;
+    this.N = Lx * Ly;
+    console.log("Lx=", Lx, "Ly=", Ly, "N=", this.N);
 
-    var w=3;
-    var canvas = document.getElementById('ising');
-    var context = canvas.getContext('2d');
-    canvas.width = canvas.height = l*(w+1)-1;
+    this.Spin = new Int8Array(this.N);
+    this.NN0 = new Int32Array(this.N);
+    this.NN1 = new Int32Array(this.N);
+    this.NN2 = new Int32Array(this.N);
+    this.NN3 = new Int32Array(this.N);
 
-    var spin = new Array(n);
-    var spinBefore = new Array(n);
-    for(var i=0;i<n;++i) {
-        spin[i] = (Math.random()<0.5) ? 1 : -1;
-        spinBefore[i] = 0;
+    this.Spin.forEach((val, idx) => {
+      this.Spin[idx] = Math.random() < 0.5 ? 1 : -1;
+
+      let x, y;
+      [x, y] = this.get_coordinate(idx);
+      this.NN0[idx] = this.get_index(x + 1, y);
+      this.NN1[idx] = this.get_index(x, y + 1);
+      this.NN2[idx] = this.get_index(x - 1, y);
+      this.NN3[idx] = this.get_index(x, y - 1);
+    });
+
+    // For Swendsen-Wang algorithm
+    this.Parent = new Int32Array(this.N);
+
+    // For Swendsen-Wang algorithm
+    this.Stack = new Array();
+  }
+
+  get_coordinate(i) {
+    return [i % this.Lx, Math.floor(i / this.Lx)];
+  }
+
+  get_index(x, y) {
+    x = (x + this.Lx) % this.Lx;
+    y = (y + this.Ly) % this.Ly;
+    return x + y * this.Lx;
+  }
+
+  is_up(i) {
+    return this.Spin[i] > 0;
+  }
+
+  update(i) {
+    if (i == 1) {
+      this.swendsen_wang();
+    } else if (i == 2) {
+      this.wolff();
+    } else {
+      this.heat_bath();
     }
+  }
 
-    var cluster = new Array(n);
+  ////////////////////////////////////////
+  heat_bath() {
+    for (let i = 0; i < this.N; i += 2) {
+      this.single_flip(i);
+    }
+    for (let i = 1; i < this.N; i += 2) {
+      this.single_flip(i);
+    }
+  }
 
-    function draw() {
-        context.fillStyle = '#3366cc';
-        for(var i=0;i<l;++i) {
-            for(var j=0;j<l;++j) {
-                var idx = i*l+j;
-                if(spin[idx]==1 && spinBefore[idx]!=1)
-                    context.fillRect(j*(w+1),i*(w+1),w,w);
-            }
+  single_flip(i) {
+    let s = this.Spin[i];
+    let s_nn =
+      this.Spin[this.NN0[i]] +
+      this.Spin[this.NN1[i]] +
+      this.Spin[this.NN2[i]] +
+      this.Spin[this.NN3[i]];
+    let prob = 1.0 / (1.0 + Math.exp((2.0 * s * s_nn) / this.T));
+    if (Math.random() < prob) {
+      this.Spin[i] *= -1;
+    }
+  }
+
+  ////////////////////////////////////////
+  swendsen_wang() {
+    const prob = 1.0 - Math.exp(-2.0 / this.T);
+    this.Parent.forEach((_, i) => {
+      this.Parent[i] = i;
+    });
+    this.Spin.forEach((_, i) => {
+      this.connect(i, this.NN0[i], prob);
+      this.connect(i, this.NN1[i], prob);
+    });
+    this.Spin.forEach((_, i) => {
+      let root = this.find(i);
+      if (i == root) {
+        this.Spin[i] = Math.random() < 0.5 ? 1 : -1;
+      } else {
+        this.Spin[i] = this.Spin[root];
+      }
+    });
+  }
+
+  find(i) {
+    // Find with path splitting
+    while (i != this.Parent[i]) {
+      [i, this.Parent[i]] = [this.Parent[i], this.Parent[this.Parent[i]]];
+    }
+    return i;
+  }
+
+  unite(i, j) {
+    // Unite by index
+    i = this.find(i);
+    j = this.find(j);
+    if (i == j) {
+      return;
+    }
+    if (i > j) {
+      [i, j] = [j, i];
+    }
+    this.Parent[j] = i;
+  }
+
+  connect(i, j, prob) {
+    if (this.Spin[i] == this.Spin[j] && Math.random() < prob) {
+      this.unite(i, j);
+    }
+  }
+
+  ////////////////////////////////////////
+  wolff() {
+    let count = 0;
+
+    // This implementation is only for visualization.
+    // In the Wolff algorithm, we need to measure the average size of a cluster
+    // and determine the number of flips in one Monte Carlo step.
+    while (count < this.N / 5) {
+      count += this.flip_cluster();
+    }
+  }
+
+  flip_cluster() {
+    const prob = 1.0 - Math.exp(-2.0 / this.T);
+    let i = Math.floor(Math.random() * this.N) % this.N;
+    const s = this.Spin[i];
+    let count = 1;
+    this.Spin[i] *= -1;
+    this.Stack.push(i);
+
+    while (this.Stack.length > 0) {
+      i = this.Stack.pop();
+      [this.NN0[i], this.NN1[i], this.NN2[i], this.NN3[i]].forEach((j) => {
+        if (s == this.Spin[j] && Math.random() < prob) {
+          count++;
+          this.Spin[j] *= -1;
+          this.Stack.push(j);
         }
-        context.fillStyle = '#ffffff';
-        for(var i=0;i<l;++i) {
-            for(var j=0;j<l;++j) {
-                var idx = i*l+j;
-                if(spin[idx]==-1 && spinBefore[idx]!=-1)
-                    context.fillRect(j*(w+1),i*(w+1),w,w);
-            }
-        }
-        for(var i=0;i<n;++i) spinBefore[i] = spin[i];
+      });
     }
 
-    function updateSpin() {
-        if(algorithm==0) heatBath();
-        else if(algorithm==1) swendsenWang();
-        else if(algorithm==2) wolff();
-        else randomUpdate();
-    }
-
-    function heatBath() {
-        interval = 40;
-        // 1MCS update
-        for(var k=0;k<n;++k) {
-            var i = randomInt(0,n);
-            var s0 = spin[i];
-            var s1 = spin[(i+1)%n];
-            var s2 = spin[(i-1+n)%n];
-            var s3 = spin[(i+l)%n];
-            var s4 = spin[(i-l+n)%n];
-            var prob = 1.0/(1.0+Math.exp(2.0*s0*(s1+s2+s3+s4)/temp))
-            if(Math.random()<prob) spin[i] = -s0;
-        }
-    }
-
-    function swendsenWang() {
-        interval = 100;
-        var prob = 1.0-Math.exp(-2.0/temp);
-        makeCluster(prob);
-        for(var i=0;i<n;++i) {
-            var root = cluster[i];
-            if(i == root) {
-                spin[i] = (Math.random()<0.5) ? 1 : -1;
-            } else {
-                spin[i] = spin[root];
-            }
-        }
-    }
-
-    function wolff() {
-        interval = 100;
-        var prob = 1.0-Math.exp(-2.0/temp);
-        var count = 0;
-
-        makeCluster(prob);
-        while(count<n/5) {
-            var updateCluster = cluster[randomInt(0,n)];
-            for(var i=0;i<n;++i) {
-                if(cluster[i] == updateCluster) {
-                    spin[i] = -spin[i];
-                    count++;
-                }
-            }
-        }
-    }
-
-    function findRoot(i) {
-        var child = i;
-        var parent = cluster[child];
-        while(child != parent) {
-            child = parent;
-            parent = cluster[child];
-        }
-        return child;
-    }
-
-    function connect(i,j) {
-        var ri = findRoot(i);
-        var rj = findRoot(j);
-        var root = (ri<rj) ? ri : rj;
-        var child = i;
-        while(child != root) {
-            var parent = cluster[child];
-            cluster[child] = root;
-            child = parent;
-        }
-        var child = j;
-        while(child != root) {
-            var parent = cluster[child];
-            cluster[child] = root;
-            child = parent;
-        }
-    }
-
-    function makeCluster(prob) {
-        for(var i=0;i<n;++i) cluster[i]=i;
-        for(var i=0;i<n;++i) { // horizontal bonds
-            var j = (i+1)%n;
-            if(spin[i]==spin[j] && Math.random()<prob) connect(i,j);
-        }
-        for(var i=0;i<n;++i) { // vartical bonds
-            var j = (i+l)%n;
-            if(spin[i]==spin[j] && Math.random()<prob) connect(i,j);
-        }
-        for(var i=0;i<n;++i) {
-            cluster[i] = findRoot(i);
-        }
-    }
-
-    function randomInt(min,max) {
-        return Math.floor(Math.random() * (max-min))+min;
-    }
-
-    function magnetization() {
-        var m = 0.0;
-        for(var i=0;i<n;++i) {
-            m += spin[i];
-        }
-        return m/n;
-    }
-
-    function randomUpdate() {
-        interval = 200;
-        for(var i=0;i<n;++i) {
-            if(Math.random()<0.5) spin[i] = 1;
-            else spin[i] = -1;
-        }
-    }
-
-    function update() {
-        updateSpin();
-        draw();
-        graph.addPoint(temp,magnetization(),algorithm);
-    }
-
-    function loop() {
-        var now = Date.now();
-        if(now-basetime>interval) {
-            basetime = now;
-            update();
-        }
-        timerID = requestAnimationFrame(loop);
-    }
-
-    return {
-        start: function() {
-            if(!running) loop();
-            running = true;
-        },
-        stop: function() {
-            cancelAnimationFrame(timerID);
-            running = false;
-        },
-        step: function() {
-            cancelAnimationFrame(timerID);
-            running = false;
-            update();
-        },
-        setTemp: function(t) {temp=t;},
-        setTempTc: function() {temp=tc;},
-        getTemp: function() {return temp;},
-        setAlgorithm: function(a) {algorithm=a;},
-        getAlgorithm: function() {return algorithm;},
-    }
+    return count;
+  }
 }
 
-function Graph() {
-    var canvas = document.getElementById("graph");
-    var context = canvas.getContext("2d");
-    var offset = 7.5;
-    var w = 11;
-    var t_min = 0.5;
-    var t_del = 0.1;
+class DrawIsing {
+  constructor(id, full) {
+    this.id = id;
+    this.full = full;
 
-    var width = canvas.width = 400-1;
-    var height = canvas.height = w*10+offset*2;
-
-    reset();
-
-    function x(t) {return w*(t-t_min)/t_del+offset;}
-    function y(m) {return w*(1.0-m)/0.2+offset;}
-    function t(x) {
-        var t = t_del*(x-offset)/w+t_min;
-        t = Math.round(t*100)/100;
-        if(t<0.5) t=0.5;
-        if(t>4.0) t=4.0;
-        return t;
-    }
-
-    function addPoint(t,m,a) {
-        if(a==0) context.fillStyle = "rgba(153,153,255,0.5)";
-        else if(a==1) context.fillStyle = "rgba(153,255,153,0.5)";
-        else if(a==2) context.fillStyle = "rgba(255,153,153,0.5)";
-        else context.fillStyle = "rgba(255,255,255,0.5)";
-        context.beginPath();
-        context.arc( x(t), y(m), 2,0,2*Math.PI,true);
-        context.fill();
-    }
-
-    function reset() {
-        context.fillStyle = "#161616";
-        context.fillRect(0,0,width,height)
-
-        context.lineWidth = 1;
-
-        context.strokeStyle = "#333333";
-        for(var i=0;i<36;++i)  {
-            context.beginPath();
-            context.moveTo(offset+i*w, 0);
-            context.lineTo(offset+i*w, height);
-            context.stroke();
-        }
-        for(var i=0;i<11;++i) {
-            context.beginPath();
-            context.moveTo(0, offset+i*w);
-            context.lineTo(width, offset+i*w);
-            context.stroke();
-        }
-
-        context.strokeStyle = "#666666";
-        for(var i=0;i<36;i+=5)  {
-            context.beginPath();
-            context.moveTo(offset+i*w, 0);
-            context.lineTo(offset+i*w, height);
-            context.stroke();
-        }
-        for(var i=0;i<11;i+=5) {
-            context.beginPath();
-            context.moveTo(0, offset+i*w);
-            context.lineTo(width, offset+i*w);
-            context.stroke();
-        }
-
-        context.strokeStyle = "#cccccc";
-        context.beginPath();
-        context.moveTo(0, height/2);
-        context.lineTo(width, height/2);
-        context.stroke();
-    }
-
-    return {
-        addPoint: addPoint,
-        reset: reset,
-        getT: t
-    }
-}
-
-var ising = Ising();
-var graph = Graph();
-
-$(function () {
-    var initT = 2.27, timerID;
-
-    function setT(t) {
-        var t = Math.round(t*100)/100;
-        $( "#temperature_value" ).text( t );
-        ising.setTemp(t);
-        $("#temperature").slider("setValue", t);
-    }
-
-    function rotateAlgorithm() {
-        var a=ising.getAlgorithm();
-        a = (a+1)%3;
-        ising.setAlgorithm(a);
-        $("#algorithm").selectpicker("val",a);
-    }
-
-    function cool() {
-        var t = ising.getTemp();
-        var dt = 0.1, interval = 1000;
-        if(t>2.6) {
-            dt = 0.1; interval = 1000;
-        } else if(t>2.1) {
-            dt = 0.05; interval = 4000;
-        } else {
-            dt = 0.1; interval = 1000;
-        } 
-
-        t = Math.round( (t-dt)*100 )/100;
-        if (t<0.5) {
-            t = 4.1;
-            rotateAlgorithm();
-        }
-
-        setT(t);
-        timerID = setTimeout(cool, interval);
-        ising.start();
-    }
+    const color_back = "#ffffff";
+    const color_spin = "#6191f2";
+    const lw = 0.2;
+    const length = 4;
+    let shape = new createjs.Shape();
+    shape.graphics
+      .ss(lw)
+      .beginStroke(color_back)
+      .beginFill(color_spin)
+      .drawRect(0, 0, length, length);
+    shape.cache(0, 0, length, length);
+    this.cell_image = shape.cacheCanvas;
+    this.length = length;
     
-    function stopCool() {
-        clearTimeout(timerID);
-        if ($("#auto").hasClass("active")) {
-            $("#auto").button("toggle");
-        }
+    this.stage = new createjs.StageGL(this.id);
+    if (this.full) {
+      this.stage.canvas.width = window.innerWidth;
+      this.stage.canvas.height = window.innerHeight;
+      this.stage.updateViewport(
+        this.stage.canvas.width,
+        this.stage.canvas.height
+      );
     }
 
-    var t_slider = $("#temperature").slider({
-        min: 0.5,
-        max: 4.0,
-        step: 0.01,
-        value: initT
+    this.stage.setClearColor(color_back);
+    this.stage.update();
+
+    let cx = this.stage.canvas.width;
+    let cy = this.stage.canvas.height;
+    let Lx = Math.floor(cx / this.length) + 1;
+    let Ly = Math.floor(cy / this.length) + 1;
+    let N = Lx * Ly;
+
+    this.ising = new Ising(Lx, Ly, 2.27);
+    this.algorithm = 0;
+    this.framerate = [20, 5, 5];
+
+    this.ising.Spin.forEach((_, i) => {
+      let image = new createjs.Bitmap(this.cell_image);
+      this.stage.addChild(image);
+      let x, y;
+      [x, y] = this.ising.get_coordinate(i);
+      image.x = this.length * x;
+      image.y = this.length * y;
+    })
+
+    createjs.Ticker.framerate = this.framerate[this.algorithm];
+    createjs.Ticker.addEventListener("tick", () => {
+      this.update();
     });
 
-    t_slider.on("slide", function(e) {
-        setT( e.value );
+    let slider = document.getElementById("temperature");
+    if (slider != null) {
+      let temp_text = document.getElementById("temperature_text");
+      slider.value = this.ising.T;
+      slider.addEventListener("input", (e) => {
+        this.ising.T = e.target.value;
+        temp_text.innerText = e.target.value;
+      });
+    }
+
+    let select_algorithm = document.getElementById("algorithm");
+    if (select_algorithm != null) {
+      select_algorithm.addEventListener("input", (e) => {
+        this.change_algorithm(e.target.value);
+      });
+    }
+
+    if (this.full) {
+      this.timeoutID = 0;
+      window.addEventListener("resize", () => {
+        if (this.timeoutID) return;
+        this.timeoutID = setTimeout(() => {
+          this.timeoutID = 0;
+          this.resize();
+        }, 500);
+      });
+    }
+  }
+
+  draw() {
+    this.ising.Spin.forEach((e, i) => {
+      this.stage.children[i].visible = this.ising.is_up(i);
     });
-    t_slider.prev().on("mouseup",function() {
-        setT( t_slider.slider("getValue") );
+    this.stage.update();
+  }
+
+  update() {
+    this.draw();
+    this.ising.update(this.algorithm);
+  }
+
+  change_algorithm(i) {
+    this.algorithm = i;
+    createjs.Ticker.framerate = this.framerate[i];
+  }
+
+  resize() {
+    const cx = window.innerWidth;
+    const cy = window.innerHeight;
+    this.stage.canvas.width = cx;
+    this.stage.canvas.height = cy;
+    this.stage.updateViewport(cx, cy);
+    const Lx = Number.parseInt(cx / this.length) + 1;
+    const Ly = Number.parseInt(cy / this.length) + 1;
+    const N = Lx * Ly;
+
+    for (let i = this.stage.numChildren; i < N; ++i) {
+      let image = new createjs.Bitmap(this.cell_image);
+      this.stage.addChildAt(image, i);
+    }
+    this.stage.children.forEach((e) => {
+      e.visible = false;
     });
 
-    $("#start").on("click", ising.start);
-    $("#stop" ).on("click", function() {
-        stopCool();
-        ising.stop();
+    this.ising.resize(Lx, Ly);
+    this.ising.Spin.forEach((_, i) => {
+      let x, y;
+      [x, y] = this.ising.get_coordinate(i);
+      this.stage.children[i].x = this.length * x;
+      this.stage.children[i].y = this.length * y;
     });
-    $("#step" ).on("click", function() {
-        stopCool();
-        ising.step();
-    });
+  }
+}
 
-    $("#algorithm").on("change", function() {
-        ising.setAlgorithm( $(this).val() );
-    });
-
-    $("#graph").on("click", function(e) {
-        var x  = (e.offsetX || e.clientX - $(e.target).offset().left); // polyfill for firefox
-        setT( graph.getT(x) );
-    });
-
-    $("#clear").on("click", graph.reset);
-
-    $("#auto").on("click", function(e) {
-        $(this).button("toggle");
-        if($(this).hasClass("active")) {
-            cool();
-        } else {
-            stopCool();
-        }
-    });
-
-    setT(initT);
-    ising.start();
+window.addEventListener("load", () => {
+  new DrawIsing("ising", true);
 });
